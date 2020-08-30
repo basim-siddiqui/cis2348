1bf86ad916cc2befe1283198c5d9a09c54cf0a41
basim-siddiqui <basiddiqui@uh.edu>
2020-08-30 14:38:35 -0500

    step 1

--- homework1/main.py
+++ homework1/main.py
@@ -1,1 +1,7 @@
+# Basim Siddiqui
+# PSID: 1517778
 
+userNum = int(input())
+userNumSquared = userNum**2# Bug here; fix it when instructed
+
+print(userNumSquared, end=' ') # Output formatting issue here; fix it when instructed
