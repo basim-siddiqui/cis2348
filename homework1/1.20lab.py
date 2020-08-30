6f0be5338ca9328581d993a0e0197487fbf11815
basim-siddiqui <basiddiqui@uh.edu>
2020-08-30 15:09:58 -0500

    whatever

--- homework1/1.20lab.py
+++ homework1/1.20lab.py
@@ -1,1 +1,10 @@
+# Basim Siddiqui
+# PSID: 1517778
 
+user_num1 = int(input())
+print(user_num1)
+print(user_num1 * user_num1)
+print(user_num1 * user_num1 * user_num1)
+user_num2 = int(input())
+print(user_num1 + user_num2)
+print(user_num1 * user_num2)
