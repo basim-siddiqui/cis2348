c8f0fcd71600cf815dc33bfae42206662c3ccf71
basim-siddiqui <basiddiqui@uh.edu>
2020-08-30 15:13:01 -0500

    done

--- homework1/main.py
+++ homework1/main.py
@@ -6,10 +6,3 @@ userNumSquared = userNum**2# Bug here; fix it when instructed
 
 print(userNumSquared) # Output formatting issue here; fix it when instructed
 
-user_num1 = int(input())
-print(user_num1)
-print(user_num1 * user_num1)
-print(user_num1 * user_num1 * user_num1)
-user_num2 = int(input())
-print(user_num1 + user_num2)
-print(user_num1 * user_num2)
