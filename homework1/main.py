f474ea72fb85b665820320ed61315a35f05f069a
basim-siddiqui <basiddiqui@uh.edu>
2020-08-30 15:07:04 -0500

    2nd lab

--- homework1/main.py
+++ homework1/main.py
@@ -4,4 +4,12 @@
 userNum = int(input())
 userNumSquared = userNum**2# Bug here; fix it when instructed
 
-print(userNumSquared, end=' ') # Output formatting issue here; fix it when instructed
+print(userNumSquared) # Output formatting issue here; fix it when instructed
+
+user_num1 = int(input())
+print(user_num1)
+print(user_num1 * user_num1)
+print(user_num1 * user_num1 * user_num1)
+user_num2 = int(input())
+print(user_num1 + user_num2)
+print(user_num1 * user_num2)
