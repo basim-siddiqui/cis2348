177aba9d202b5d69295a14579e9d268df2eca9b2
basim-siddiqui <basiddiqui@uh.edu>
2020-08-30 14:09:30 -0500

    test

--- homework1/problem1.py
+++ homework1/problem1.py
@@ -1,3 +1,27 @@
 # Basim Siddiqui
 # PSID: 1517778
-print("test")
+print("This program will calculate your age.")
+current_date = input("Enter the current date by month, day, and year.")
+birthday = input("Enter you birthday by month, day, and year.")
+
+print("Current Day Information:")
+cmonth, cday, cyear = current_date.split("/")
+print(cmonth)
+print(cday)
+print(cyear)
+
+print("Birthday Information:")
+month, day, year = birthday.split("/")
+print(month)
+print(day)
+print(year)
+
+age = int(cyear) - int(year)
+if int(cmonth) < int(month):
+    age = age - 1
+if int(cmonth) == int(month) and int(cday) < int(day):
+    age = age - 1
+print("You are", age, "years old.")
+
+if cmonth == month and cday == day:
+    print("Happy Birthday!")
