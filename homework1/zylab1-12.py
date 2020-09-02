fcdd7a00659a2ad85138f6f6ad7fddbf8e321a6d
basim-siddiqui <basiddiqui@uh.edu>
2020-09-02 15:06:07 -0500

    lab1.12

---
+++ homework1/zylab1-12.py
@@ -0,0 +1,7 @@
+# Basim Siddiqui
+# PSID: 1517778
+
+userNum = int(input())
+userNumSquared = userNum**2# Bug here; fix it when instructed
+
+print(userNumSquared) # Output formatting issue here; fix it when instructed
