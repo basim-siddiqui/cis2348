fe24ce66569dfd539a71ff3cd80fb753408656c6
basim-siddiqui <basiddiqui@uh.edu>
2020-09-11 15:20:56 -0500

    lab 3.18

--- homework1/zylab3-18.py
+++ homework1/zylab3-18.py
@@ -1,33 +1,14 @@
 # Basim Siddiqui
 # PSID: 1517778
 
-print("Davy's auto shop services")
-print("Oil change -- $35")
-print("Tire rotation -- $19")
-print("Car wash -- $7")
-print("Car wax -- $12\n")
-auto_service = {'Oil change': 35, 'Tire rotation': 19, 'Car wash': 7, 'Car wax': 12, '-': 'No service'}
-
-service1 = input("Select first service:\n")
-service2 = input("Select second service:\n")
-
-print("\nDavy's auto shop invoice\n")
-if service1 != '-' and service2 != '-':
-    print("Service 1:", '{},'.format(service1), '${}'.format(auto_service[service1]))
-    print("Service 2:", '{},'.format(service2), '${}'.format(auto_service[service2]))
-    total_price = auto_service[service1] + auto_service[service2]
-    print('\nTotal:', '${}'.format(total_price))
-elif service1 == '-' and service2 != '-':
-    print("Service 1:", auto_service[service1])
-    print("Service 2:", '{},'.format(service2), '${}'.format(auto_service[service2]))
-    total_price = auto_service[service2]
-    print('\nTotal:', '${}'.format(total_price))
-elif service1 != '-' and service2 == '-':
-    print("Service 1:", '{},'.format(service1), '${}'.format(auto_service[service1]))
-    print("Service 2:", auto_service[service2])
-    total_price = auto_service[service1]
-    print('\nTotal:', '${}'.format(total_price))
-else:
-    print("Service 1:", auto_service[service1])
-    print("Service 2:", auto_service[service2])
-    print('\nTotal:', '${}'.format(0))
+import math
+height = input("Enter wall height (feet):\n")
+width = input("Enter wall width (feet):\n")
+print("Wall area:", int(height) * int(width), "square feet")
+paint = int(height) * int(width) / 350.0
+print("Paint needed:", '{:.2f}'.format(paint), 'gallons')
+cans = math.ceil(paint)
+print("Cans needed:", cans, 'can(s)\n')
+colors = {'red': 35, 'blue': 25, 'green': 23}
+paint_color = input("Choose a color to paint the wall:\n")
+print("Cost of purchasing", paint_color, 'paint:', '${}'.format(colors[paint_color] * cans))
