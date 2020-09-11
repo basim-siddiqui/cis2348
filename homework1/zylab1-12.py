482f946e138cdf3cbf04830a47cfd70d71545d30
basim-siddiqui <70299419+basim-siddiqui@users.noreply.github.com>
2020-09-11 15:40:16 -0500

    lab 1.12

--- homework1/zylab1-12.py
+++ homework1/zylab1-12.py
@@ -2,6 +2,6 @@
 # PSID: 1517778
 
 userNum = int(input())
-userNumSquared = userNum**2# Bug here; fix it when instructed
+userNumSquared = userNum**2
 
-print(userNumSquared) # Output formatting issue here; fix it when instructed
+print(userNumSquared)
