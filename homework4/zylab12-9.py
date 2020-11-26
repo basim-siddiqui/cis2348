26793e81e2fa6f14aaa5e44a89386b7b10110db8
basim-siddiqui <basiddiqui@uh.edu>
2020-11-25 18:33:17 -0600

    zylab 12.9

---
+++ homework4/zylab12-9.py
@@ -0,0 +1,16 @@
+# Basim Siddiqui
+# PSID: 1517778
+
+# Split input into 2 parts: name and age
+parts = input().split()
+name = parts[0]
+while name != '-1':
+    try:
+        age = int(parts[1]) + 1
+    except ValueError as ex:
+        age = 0
+
+    # Get next line
+    print('{} {}'.format(name, age))
+    parts = input().split()
+    name = parts[0]
